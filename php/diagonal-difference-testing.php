61150661e2746bd9aebf31d36664cd73f363a6a8
thiago olivier <thiagoolivier53@gmail.com>
2023-01-29 10:17:05 -0400

    Tested both functions and passed the hacker rank test.

--- php/diagonal-difference-testing.php
+++ php/diagonal-difference-testing.php
@@ -1,5 +1,8 @@
 <?php
 
+/* This function takes an array where the first index is the 2D array size. 
+For example, if the given array is 4x4 -> $array[0] = 4 */
+
 $data = array(
     0 => 3,
     1 => array(11,2,4),
@@ -13,14 +16,14 @@ function diagonalDifference($array) {
     $leftToRight = 0;
     $rightToLeft = 0;
 
-    for ($i = 1; $i <= $arraySize-1; $i++) {
+    for ($i = 1; $i <= $arraySize; $i++) {
         $leftToRight += $array[$i][$i-1];
     }
 
 
     $j = $arraySize-1;
 
-    for ($i = 1; $i <= $arraySize-1; $i++) {
+    for ($i = 1; $i <= $arraySize; $i++) {
         $rightToLeft += $array[$i][$j];
         $j--;
     }
