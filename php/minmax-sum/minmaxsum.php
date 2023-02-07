5b99efbd961bf8a1950ef9ebfea3f05a7ac76255
thiago oliveira <thiagoolivier53@gmail.com>
2023-02-07 12:01:00 -0400

    Finished the exercise miniMaxSum from Hacker Rank.

--- php/minmax-sum/minmaxsum.php
+++ php/minmax-sum/minmaxsum.php
@@ -1,27 +1,32 @@
 <?php
 
-$arr = array(1, 2, 3, 4, 5);
+/*
+Function description: takes an array of integers and outputs the 
+sum without the maximum value and another without the mininum value. 
+*/
+
+//sample arrays (call the function parsing them).
+$arr1 = array(5,5,5,5,5);
+$arr2 = array(1,2,3,4,5);
+$arr3 = array(10,30,50,60,90);
+$arr4 = array(140638725,436257910,953274816,734065819,362748590);
+
 
 function minMaxSum ($arr)
 {
+    $maxNumber = max($arr);
+    $minNumber = min($arr);
     
-    $minNumber = $arr[0];
-    $maxNumber = $arr[0];
-
-    foreach ($arr as $item) {
-        
-        if($item < $minNumber) {
-            $minNumber = $item;
-        }
-
-        if($item > $maxNumber) {
-            $maxNumber = $item;
-        }
-
-    }
-
+    $maxTotal = array_sum($arr) - $maxNumber;
+    $minTotal = array_sum($arr) - $minNumber;
+     
+    echo $maxTotal."&nbsp";
+    echo $minTotal;
     
 }
+
+
+minMaxSum($arr4);
 
 
 ?>
