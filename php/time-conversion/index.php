64aca28b64033e60728b00327ca0745b12f7adb8
thiago oliveira <thiagoolivier53@gmail.com>
2023-02-14 10:48:54 -0400

    Created the time-conversion solution.

--- php/time-conversion/index.php
+++ php/time-conversion/index.php
@@ -1,19 +1,50 @@
 <?php
+/*
+This function gets the time with AM or PM and returns as "HH:mm:ii"
+*/
 
-$input = "01:01:00PM";
 
-if (str_contains($input, "PM"))
+$sample = "12:45:54PM";
+
+function timeConversion($input)
 {
-    $woLetters = substr($input, 0, 8);
-
-    $hour = (string)(substr($input, 0, 2) + 12);
     
-    if($hour === '24') {
-        $hour = '00';
+    if (str_contains($input, "PM"))
+    {
+        $woLetters = substr($input, 0, 8);
+        
+        $verifyHour = (string)(substr($input, 0, 2));
+        
+        if ($verifyHour === '12') {
+            return $woLetters;
+        }
+                
+        $hour = (string)(substr($input, 0, 2) + 12);
+        
+        if($hour === '24') {
+            $hour = '00';
+        }
+
+        if($hour <= '23' && $hour >= '00') {
+            return substr_replace($woLetters, $hour, 0, 2);
+        }
+        
+    } else {
+
+        $woLetters = substr($input, 0, 8);
+        
+        $hour = (substr($input, 0, 2));
+
+        if($hour === '12') {
+            $hour = '00';
+        }
+
+        return substr_replace($woLetters, $hour, 0, 2);
+
     }
 
-    echo substr_replace($woLetters, $hour, 0, 2);
 }
 
+echo timeConversion($sample);
 
 ?>
